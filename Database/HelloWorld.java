cc6dd470ff709aec06eb24d6b467442b559bfc3c
Nishant Gaurav <ng19nishant@gmail.com>
2023-10-04 23:38:23 +0530

    Added information

---
+++ Database/HelloWorld.java
@@ -0,0 +1,10 @@
+// LANG: Java
+// AUTHOR: Nishant Gaurav
+// GITHUB: https://github.com/Imbiber
+
+package Database;
+public class HelloWorld {
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+    }
+}
