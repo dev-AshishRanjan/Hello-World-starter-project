95800a9fe82a519faf3b96f0af007c53ab3daa63
Abhinav Prajapati <98694384+abhinav89908@users.noreply.github.com>
2023-10-15 19:41:02 +0530

    Update Hello_World_Abhi.cpp
    I have corrected my code and it is perfect now. The code has proper comments and it prints 'Hello World'

--- Database/Hello_World_Abhi.cpp
+++ Database/Hello_World_Abhi.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
-using namesapce std;
+using namespace std;
 int main(){
 	// LANG: C++
 	// AUTHOR: Abhinav Prajapati
 	// GITHUB: https://github.com/abhinav89908
 	cout<<"Hello World!";
+	// The above code will print 'Hello World' in the console
+	// cout is an instance of ostream class used to produce output
 	return 0;
+	// We are returning 0 to notify that the code executed successfully
 }
